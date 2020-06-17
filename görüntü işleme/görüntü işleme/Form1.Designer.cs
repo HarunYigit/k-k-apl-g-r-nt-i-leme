c09a51a09ee7b240be841e64473c7e41428ebdbb
HarunYigit <63460368+HarunYigit@users.noreply.github.com>
2020-06-17 15:08:38 +0300

    harun yiğit
    küçük çaplı bir görüntü işleme ama çok küçük çaplı :)

---
+++ görüntü işleme/görüntü işleme/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace görüntü_işleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(175, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::görüntü_işleme.Properties.Resources.IMG_20200607_183805___Kopya;
+            this.pictureBox1.Location = new System.Drawing.Point(579, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(752, 405);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Resim değişmek için resme tıklayınız";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 451);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ görüntü işleme/görüntü işleme/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace görüntü_işleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static Bitmap bit = new Bitmap(400, 400);
+        static Graphics grafik = Graphics.FromImage(bit);
+        static Bitmap yenibit = new Bitmap(400, 400);
+        static Graphics yenigrafik = Graphics.FromImage(bit);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();   
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                grafik.Clear(Color.White);
+
+                grafik.DrawImage(pictureBox1.Image, 0, 0, 400, 400);
+                button1.Enabled = true;
+                timer1.Stop();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread a = new Thread(yap); a.Start();
+        }
+        public void yap()
+        {
+            int data = 0;
+            for (int y = 0; y < 398; y++)
+            {
+                for (int x = 0; x < 398; x++)
+                {
+                    Color renk = bit.GetPixel(x, y);
+                    Color srenk = bit.GetPixel(x + 1, y);
+                    if (hatvarmı(renk, srenk, 800000) == true)
+                    {
+                        yenibit.SetPixel(x, y, Color.Red);
+                        data += 1;
+                    }
+                    else
+                    {
+                        yenibit.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+                yansıt();
+            
+        }
+        public void yansıt() {       
+            pictureBox2.Image = yenibit;
+           // System.IO.File.WriteAllText("c:\\users\\oyn\\desktop\\ghafıza\\1.txt", bütündata.ToString(), System.Text.Encoding.UTF8);
+
+        }
+        public Boolean hatvarmı(Color color1,Color color2,int bakışaçısı)
+        {
+
+            if (Math.Abs(color1.ToArgb() - color2.ToArgb()) > bakışaçısı)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void msgbox(String mesaj)
+        {
+            MessageBox.Show(mesaj);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = openFileDialog1.FileName;
+                button1.Enabled = false;
+                timer1.Start();
+            }
+        }
+    }
+}
